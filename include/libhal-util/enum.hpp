001d36e583f0bc2699ee7456bd7ed9f0c5dcda47
Khalil Estell <kammcecorp@gmail.com>
2022-12-10 17:32:26 -0800

    :white_check_mark: Fix enum.hpp & add tests for enum class

--- include/libhal-util/enum.hpp
+++ include/libhal-util/enum.hpp
@@ -1,67 +1,10 @@
 #pragma once
 
-#include <ios>
-#include <string_view>
 #include <type_traits>
 
 #include <libhal/units.hpp>
 
 namespace hal {
-
-/**
- * @brief Get string representation of the work state.
- *
- * @param p_state - work state
- * @return std::string_view - string representation
- */
-constexpr std::string_view to_string(work_state p_state)
-{
-  switch (p_state) {
-    case work_state::in_progress:
-      return "in progress";
-    case work_state::failed:
-      return "failed";
-    case work_state::finished:
-      return "finished";
-    default:
-      return "unknown work state";
-  }
-}
-
-/**
- * @brief Indicate if a work_state is terminal, meaning the worker has finished
- * its job.
- *
- * @param p_state - the work state to check
- * @return true - work state is either finished or failed
- * @return false - work state is still in progress
- */
-constexpr bool terminated(work_state p_state)
-{
-  return p_state == work_state::finished || p_state == work_state::failed;
-}
-
-/**
- * @brief print work_state type using ostreams
- *
- * Meant for unit testing, testing and simulation purposes
- * C++ streams, in general, should not be used for any embedded project that
- * will ever have to be used on an MCU due to its memory cost.
- *
- * @tparam CharT - character type
- * @tparam Traits - ostream traits type
- * @param p_ostream - the ostream
- * @param p_state - object to convert to a string
- * @return std::basic_ostream<CharT, Traits>& - reference to the ostream
- */
-template<class CharT, class Traits>
-inline std::basic_ostream<CharT, Traits>& operator<<(
-  std::basic_ostream<CharT, Traits>& p_ostream,
-  const work_state& p_state)
-{
-  return p_ostream << to_string(p_state);
-}
-
 /**
  * @brief concept for enumeration types
  *
---
+++ tests/enum.test.cpp
@@ -0,0 +1,23 @@
+#include <libhal-util/enum.hpp>
+
+#include <boost/ut.hpp>
+
+namespace hal {
+boost::ut::suite enum_test = []() {
+  using namespace boost::ut;
+
+  "hal::value(enum)"_test = []() {
+    constexpr int expected1 = 1;
+    constexpr int expected2 = 20;
+
+    enum class test_enum
+    {
+      enum_value1 = expected1,
+      enum_value2 = expected2,
+    };
+
+    expect(that % expected1 == value(test_enum::enum_value1));
+    expect(that % expected2 == value(test_enum::enum_value2));
+  };
+};
+}  // namespace hal
