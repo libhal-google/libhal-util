641bd582588e620917f3bf96046a2709a298f275
Khalil Estell <kammce@google.com>
2023-04-16 07:31:34 -0700

    :page_facing_up: Add appropriate licensing

--- conanfile.py
+++ conanfile.py
@@ -1,3 +1,19 @@
+#!/usr/bin/python
+#
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from conan import ConanFile
 from conan.tools.cmake import CMake, cmake_layout
 from conan.tools.files import copy
@@ -9,9 +25,9 @@ import os
 required_conan_version = ">=1.50.0"
 
 
-class LibhalUtilConan(ConanFile):
+class libhal_util_conan(ConanFile):
     name = "libhal-util"
-    version = "1.0.0"
+    version = "1.1.0"
     license = "Apache-2.0"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://libhal.github.io/libhal"
@@ -54,7 +70,7 @@ class LibhalUtilConan(ConanFile):
                 f"{self.name} {self.version} requires C++{self._min_cppstd}, which your compiler ({compiler}-{version}) does not support")
 
     def requirements(self):
-        self.requires("libhal/[^1.0.0]")
+        self.requires("libhal/[^1.1.1]")
         self.test_requires("boost-ext-ut/1.1.9")
 
     def layout(self):
@@ -72,12 +88,18 @@ class LibhalUtilConan(ConanFile):
             self.run(os.path.join(self.cpp.build.bindir, "unit_test"))
 
     def package(self):
-        copy(self, "LICENSE", dst=os.path.join(
-            self.package_folder, "licenses"), src=self.source_folder)
-        copy(self, "*.h", dst=os.path.join(self.package_folder, "include"),
+        copy(self,
+             "LICENSE",
+             dst=os.path.join(self.package_folder, "licenses"),
+             src=self.source_folder)
+        copy(self,
+             "*.h",
+             dst=os.path.join(self.package_folder, "include"),
              src=os.path.join(self.source_folder, "include"))
-        copy(self, "*.hpp", dst=os.path.join(self.package_folder,
-             "include"), src=os.path.join(self.source_folder, "include"))
+        copy(self,
+             "*.hpp",
+             dst=os.path.join(self.package_folder, "include"),
+             src=os.path.join(self.source_folder, "include"))
 
     def package_info(self):
         self.cpp_info.bindirs = []
