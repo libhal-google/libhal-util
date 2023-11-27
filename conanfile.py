889a180ee6ae7d9de4106489d6e9d244f1d9ee61
Khalil Estell <kammce@google.com>
2023-11-27 12:59:16 -0800

    :bug: Fix libhal usage after conan upgrade

--- conanfile.py
+++ conanfile.py
@@ -26,7 +26,7 @@ required_conan_version = ">=2.0.6"
 
 class libhal_util_conan(ConanFile):
     name = "libhal-util"
-    version = "3.0.0"
+    version = "3.0.1"
     license = "Apache-2.0"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/libhal/libhal-util"
@@ -60,11 +60,11 @@ class libhal_util_conan(ConanFile):
 
     def build_requirements(self):
         self.tool_requires("cmake/3.27.1")
-        self.tool_requires("libhal-cmake-util/3.0.0")
+        self.tool_requires("libhal-cmake-util/3.0.1")
         self.test_requires("boost-ext-ut/1.1.9")
 
     def requirements(self):
-        self.requires("libhal/[^2.0.0]")
+        self.requires("libhal/[^2.0.3]", transitive_headers=True)
 
     def layout(self):
         cmake_layout(self)
